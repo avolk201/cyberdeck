891a0029d55f74ae48da5f647c6918808ce8280a
Alana Volkov <140945097+avolk201@users.noreply.github.com>
2026-08-21 22:54:18 +1000

    .

--- setup_x11_kiosk.sh
+++ setup_x11_kiosk.sh
@@ -6,6 +6,7 @@ sudo apt-get install -y xserver-xorg xinit
 
 # To allow non-root users and systemd to start X, adjust Xwrapper
 echo "allowed_users=anybody" | sudo tee /etc/X11/Xwrapper.config > /dev/null
+echo "needs_root_rights=yes" | sudo tee -a /etc/X11/Xwrapper.config > /dev/null
 
 echo "Installing Cyberdeck OS Systemd Service..."
 sudo cp cyberdeck.service /etc/systemd/system/
